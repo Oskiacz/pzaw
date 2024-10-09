4fde7bd04ac8823efda743bc46c205a5eb845ae6
oskar a <75722832+Oskiacz@users.noreply.github.com>
2024-10-09 09:29:06 +0200

    form...?

---
+++ src/Form.js
@@ -0,0 +1,28 @@
+import './Form.css'
+import { useState } from 'react';
+
+function Form() {
+
+    function handleRadio() {
+
+    }
+
+    return(
+        <div className="form">
+            <input type="text"></input>
+            <input type="text"></input>
+            <select>
+                <option>CPU</option>
+                <option>GPU</option>
+                <option>RAM</option>
+            </select>
+
+            <label><input type="radio" name="color" value="Red" onChange={handleRadio}></input>Red</label>
+            <label><input type="radio" name="color" value="Blue" onChange={handleRadio}></input>Blue</label>
+            <label><input type="radio" name="color" value="Green"onChange={handleRadio}></input>Green</label>
+            <label><input type="radio" name="color" value="Pink" onChange={handleRadio}></input>Pink</label>
+        </div>
+    );
+}
+
+export default Form;
